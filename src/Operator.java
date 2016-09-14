71e47293a20c68e4a0da5b36d66f7f23226aa1b6
Miguel Angel <migaballengal@unal.edu.co>
2016-09-13 22:24:21 -0500

    classpath agregado

--- src/Operator.java
+++ src/Operator.java
@@ -44,6 +44,21 @@ public class Operator implements Constants {
         }
         break;
       case ROTATE_BACK:
+        if (direction == DIR_RIGHT) {
+          this.cube.frontSide.rotate(DIR_RIGHT);
+          tmp = this.cube.topSide.getRow(ROW_BOTTOM);
+          this.cube.topSide.setRow(ROW_BOTTOM, this.cube.leftSide.getColumn(COL_RIGHT), true);
+          this.cube.leftSide.setColumn(COL_RIGHT, this.cube.bottomSide.getRow(ROW_TOP), false);
+          this.cube.bottomSide.setRow(ROW_TOP, this.cube.rightSide.getColumn(COL_LEFT), true);
+          this.cube.rightSide.setRow(COL_LEFT, tmp, false);
+          } else {
+          this.cube.frontSide.rotate(DIR_LEFT);
+          tmp = this.cube.topSide.getRow(ROW_BOTTOM);
+          this.cube.topSide.setRow(ROW_BOTTOM, this.cube.rightSide.getColumn(COL_LEFT), false);
+          this.cube.rightSide.setRow(COL_LEFT, this.cube.bottomSide.getColumn(ROW_TOP), true);
+          this.cube.bottomSide.setRow(ROW_TOP, this.cube.leftSide.getColumn(COL_RIGHT), false);
+          this.cube.leftSide.setColumn(COL_RIGHT, tmp, true);
+      }
         break;
       case ROW_TOP:
         break;
