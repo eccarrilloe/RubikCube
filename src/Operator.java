41c9e01d846262d47d8594a1b0c9eee7cc0170f9
eccarrilloe <eccarrilloe@gmail.com>
2016-09-23 17:04:06 -0500

    fix(operator): operator modified

--- src/Operator.java
+++ src/Operator.java
@@ -12,16 +12,10 @@ class Operator implements Constants {
   public Operator() {
     this.initCube();
   }
-  private static long getMemory()
-   {
-       Runtime runtime = Runtime.getRuntime();
-       return runtime.totalMemory() - runtime.freeMemory();
-   }
-  public void initCube() {
-    System.gc();
-    long startMem = getMemory();
 
+  public void initCube() {
     this.cube = new byte[54];
+
     // Init sides of cube
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) this.cube[((FRONT * 9) + (i * 3) + j)]  = WHITE;
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) this.cube[((BACK * 9) + (i * 3) + j)]   = YELLOW;
@@ -29,61 +23,29 @@ class Operator implements Constants {
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) this.cube[((RIGHT * 9) + (i * 3) + j)]  = GREEN;
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) this.cube[((TOP * 9) + (i * 3) + j)]    = RED;
     for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) this.cube[((BOTTOM * 9) + (i * 3) + j)] = ORANGE;
-
-    System.gc();
-    long endMem = getMemory();
-    System.out.println("MEMORY TAKEN: " + (endMem-startMem));
   }
 
-  public byte[] disarm(int operations) {
+  public void disarm(int operations) {
     for (int i = 0; i < operations; i++) {
       int operation = ThreadLocalRandom.current().nextInt(1, OPERATIONS + 1);
       this.cube = this.operate(this.cube, operation);
     }
-    System.out.println("Cube Disarmed");
-    printCube(this.cube);
-    return this.cube;
   }
 
-  public void assemble() {
-  //  assembleBFS(cube);
-      printCube(this.cube);
-      this.getChildrens(this.cube);
-
-  }
-
-  public boolean validate(byte[] current){
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if(!(current[((FRONT * 9) + (i * 3) + j)]   == WHITE))  return false;
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if(!(current[((BACK * 9) + (i * 3) + j)]    == YELLOW)) return false;
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if(!(current[((LEFT * 9) + (i * 3) + j)]    == BLUE))   return false;
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if(!(current[((RIGHT * 9) + (i * 3) + j)]   == GREEN))  return false;
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if(!(current[((TOP * 9) + (i * 3) + j)]     == RED))    return false;
-    for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++) if (!(current[((BOTTOM * 9) + (i * 3) + j)] == ORANGE)) return false;
-
-    return true;
-  }
-
-  private byte[] deepCopy(byte[] old){
-    byte[] copy = new byte[old.length];
-    for (int i=0; i<old.length; i++) {
-       copy[i] = old[i];
+  public void assemble(int searchType) {
+    switch (searchType) {
+      case SEARCH_DFS: this.assembleDFS(); break;
+      case SEARCH_BFS: this.assembleBFS(); break;
+      case SEARCH_IDS: this.assembleIDS(); break;
+      case SEARCH_AST: this.assembleAST(); break;
+      default: break;
     }
-    return copy;
   }
 
-  public ArrayList<byte[]> getChildrens(byte[] currentCube){
-    ArrayList<byte[]> tmp = new ArrayList<>();
-    tmp.add(this.operate(currentCube, ROTATE_FRONT_DER));
-    printCube(tmp.get(0));
-    tmp.add(this.operate(currentCube, ROTATE_BACK_DER));
-    printCube(tmp.get(1));
-    tmp.add(this.operate(currentCube, ROW_TOP_DER));
-    tmp.add(this.operate(currentCube, ROW_BOTTOM_DER));
-    tmp.add(this.operate(currentCube, COL_LEFT_UP));
-    tmp.add(this.operate(currentCube, COL_RIGHT_UP));
+  private void assembleDFS() {
 
-    return tmp;
   }
+
   private byte[] assembleBFS(byte[] cube) {
     byte[] currentCube = cube.clone();
     ArrayDeque<byte[]> queue = new ArrayDeque<>();
@@ -128,6 +90,53 @@ class Operator implements Constants {
     return currentCube;
   }
 
+  private void assembleIDS() {
+
+  }
+
+  private void assembleAST() {
+
+  }
+
+  public boolean validate(byte[] current){
+    if (! this.validateSide(FRONT, WHITE, current))   return false;
+    if (! this.validateSide(BACK, YELLOW, current))   return false;
+    if (! this.validateSide(LEFT, BLUE, current))     return false;
+    if (! this.validateSide(RIGHT, GREEN, current))   return false;
+    if (! this.validateSide(TOP, RED, current))       return false;
+    if (! this.validateSide(BOTTOM, ORANGE, current)) return false;
+
+    return true;
+  }
+
+  private boolean validateSide(int side, int color, byte[] current) {
+    for (int i = 0; i < 3; i++)
+      for (int j = 0; j < 3; j++)
+        if (current[((side * 9) + (i * 3) + j)] != color)
+          return false;
+    return true;
+  }
+
+  private byte[] deepCopy(byte[] old){
+    byte[] copy = new byte[old.length];
+    for (int i=0; i<old.length; i++)
+      copy[i] = old[i];
+
+    return copy;
+  }
+
+  private ArrayList<byte[]> getChildrens(byte[] currentCube){
+    ArrayList<byte[]> tmp = new ArrayList<>();
+    tmp.add(this.operate(currentCube, ROTATE_FRONT_DER));
+    tmp.add(this.operate(currentCube, ROTATE_BACK_DER));
+    tmp.add(this.operate(currentCube, ROW_TOP_DER));
+    tmp.add(this.operate(currentCube, ROW_BOTTOM_DER));
+    tmp.add(this.operate(currentCube, COL_LEFT_UP));
+    tmp.add(this.operate(currentCube, COL_RIGHT_UP));
+
+    return tmp;
+  }
+
   public byte[] getRow(byte[] cube, int side, int row) {
     byte[] rowReturned = new byte[3];
     rowReturned[0] = this.cube[(side * 9) + (row * 3) + 0];
@@ -147,7 +156,7 @@ class Operator implements Constants {
   }
 
   public void setRow(byte[] cube, byte[] newColors, int side, int row, boolean reverse){
-    if(reverse){
+    if (reverse) {
       this.cube[(side * 9) + (row*3) + 0] = newColors[2];
       this.cube[(side * 9) + (row*3) + 1] = newColors[1];
       this.cube[(side * 9) + (row*3) + 2] = newColors[0];
@@ -177,6 +186,7 @@ class Operator implements Constants {
     byte[] temp2 = this.getColumn(cube, side, 0);
     byte[] temp3 = this.getRow(cube, side, 2);
     byte[] temp4 = this.getColumn(cube, side, 2);
+
     this.setColumn(cube, temp1, side, 2, false);
     this.setRow(cube, temp2, side, 0, true);
     this.setColumn(cube, temp3, side, 0, false);
@@ -187,56 +197,58 @@ class Operator implements Constants {
     byte[] currentCube = deepCopy(oldCube);
     byte[] tmp = new byte[3];
     switch(operation){
-      case ROTATE_FRONT_DER:
-      tmp = this.getRow(currentCube, TOP, 2);
-      this.setRow(currentCube, this.getColumn(currentCube, LEFT, 2), TOP, 2, true);
-      this.setColumn(currentCube, this.getRow(currentCube, BOTTOM, 0), LEFT, 2, false);
-      this.setRow(currentCube, this.getColumn(currentCube, RIGHT, 0), BOTTOM, 0, true);
-      this.setColumn(currentCube, tmp, RIGHT, 0, false);
-      this.rotate(currentCube, FRONT, 1); //Direction not implemented yet
-      break;
+        case ROTATE_FRONT_DER:
+        tmp = this.getRow(currentCube, TOP, 2);
+        this.setRow(currentCube, this.getColumn(currentCube, LEFT, 2), TOP, 2, true);
+        this.setColumn(currentCube, this.getRow(currentCube, BOTTOM, 0), LEFT, 2, false);
+        this.setRow(currentCube, this.getColumn(currentCube, RIGHT, 0), BOTTOM, 0, true);
+        this.setColumn(currentCube, tmp, RIGHT, 0, false);
+        this.rotate(currentCube, FRONT, 1); //Direction not implemented yet
+        break;
       case ROTATE_BACK_DER:
-      tmp = this.getRow(currentCube, TOP, 0);
-      this.setRow(currentCube, this.getColumn(currentCube, LEFT, 0), TOP, 0, true);
-      this.setColumn(currentCube, this.getRow(currentCube, BOTTOM, 2), LEFT, 0,false);
-      this.setRow(currentCube, this.getColumn(currentCube, RIGHT, 2), BOTTOM, 2, true);
-      this.setColumn(currentCube, tmp, RIGHT, 2, false);
-      this.rotate(currentCube, BACK, 1); //Direction not implemented yet
-      break;
+        tmp = this.getRow(currentCube, TOP, 0);
+        this.setRow(currentCube, this.getColumn(currentCube, LEFT, 0), TOP, 0, true);
+        this.setColumn(currentCube, this.getRow(currentCube, BOTTOM, 2), LEFT, 0,false);
+        this.setRow(currentCube, this.getColumn(currentCube, RIGHT, 2), BOTTOM, 2, true);
+        this.setColumn(currentCube, tmp, RIGHT, 2, false);
+        this.rotate(currentCube, BACK, 1); //Direction not implemented yet
+        break;
       case ROW_TOP_DER:
-      tmp = this.getRow(cube, FRONT, 0);
-      this.setRow(currentCube, this.getRow(currentCube, LEFT, 0), FRONT, 0, false);
-      this.setRow(currentCube, this.getRow(currentCube, BACK, 0), LEFT, 0, false);
-      this.setRow(currentCube, this.getRow(currentCube, RIGHT, 0), BACK, 0, false);
-      this.setRow(currentCube, tmp, RIGHT, 0, false);
-      this.rotate(currentCube, TOP, 1);
-      break;
+        tmp = this.getRow(currentCube, FRONT, 0);
+        this.setRow(currentCube, this.getRow(currentCube, LEFT, 0), FRONT, 0, false);
+        this.setRow(currentCube, this.getRow(currentCube, BACK, 0), LEFT, 0, false);
+        this.setRow(currentCube, this.getRow(currentCube, RIGHT, 0), BACK, 0, false);
+        this.setRow(currentCube, tmp, RIGHT, 0, false);
+        this.rotate(currentCube, TOP, 1);
+        break;
       case ROW_BOTTOM_DER:
-      tmp = this.getRow(currentCube, FRONT, 2);
-      this.setRow(currentCube, this.getRow(currentCube, LEFT, 2), FRONT, 2, false);
-      this.setRow(currentCube, this.getRow(currentCube, BACK, 2), LEFT, 2, false);
-      this.setRow(currentCube, this.getRow(currentCube, RIGHT, 2), BACK, 2, false);
-      this.setRow(currentCube, tmp, RIGHT, 2, false);
-      this.rotate(currentCube, BOTTOM, 1);
-      break;
+        tmp = this.getRow(currentCube, FRONT, 2);
+        this.setRow(currentCube, this.getRow(currentCube, LEFT, 2), FRONT, 2, false);
+        this.setRow(currentCube, this.getRow(currentCube, BACK, 2), LEFT, 2, false);
+        this.setRow(currentCube, this.getRow(currentCube, RIGHT, 2), BACK, 2, false);
+        this.setRow(currentCube, tmp, RIGHT, 2, false);
+        this.rotate(currentCube, BOTTOM, 1);
+        break;
       case COL_LEFT_UP:
-      tmp = this.getColumn(currentCube, FRONT, 0);
-      this.setColumn(currentCube, this.getColumn(currentCube, BOTTOM, 0), FRONT, 0,false);
-      this.setColumn(currentCube, this.getColumn(currentCube, BACK, 2), BOTTOM, 0, true);
-      this.setColumn(currentCube, this.getColumn(currentCube, TOP, 0), BACK, 2, true);
-      this.setColumn(currentCube, tmp, TOP, 0,false);
-      this.rotate(currentCube, LEFT, 1);
-      break;
+        tmp = this.getColumn(currentCube, FRONT, 0);
+        this.setColumn(currentCube, this.getColumn(currentCube, BOTTOM, 0), FRONT, 0,false);
+        this.setColumn(currentCube, this.getColumn(currentCube, BACK, 2), BOTTOM, 0, true);
+        this.setColumn(currentCube, this.getColumn(currentCube, TOP, 0), BACK, 2, true);
+        this.setColumn(currentCube, tmp, TOP, 0,false);
+        this.rotate(currentCube, LEFT, 1);
+        break;
       case COL_RIGHT_UP:
-      tmp = this.getColumn(currentCube, FRONT, 2);
-      this.setColumn(currentCube, this.getColumn(currentCube, BOTTOM, 2), FRONT, 2, false);
-      this.setColumn(currentCube, this.getColumn(currentCube, BACK, 0), BOTTOM, 2, true);
-      this.setColumn(currentCube, this.getColumn(currentCube, TOP, 2), BACK, 0, true);
-      this.setColumn(currentCube, tmp, TOP, 2, false);
-      this.rotate(currentCube, RIGHT, 1);
-      break;
+        tmp = this.getColumn(currentCube, FRONT, 2);
+        this.setColumn(currentCube, this.getColumn(currentCube, BOTTOM, 2), FRONT, 2, false);
+        this.setColumn(currentCube, this.getColumn(currentCube, BACK, 0), BOTTOM, 2, true);
+        this.setColumn(currentCube, this.getColumn(currentCube, TOP, 2), BACK, 0, true);
+        this.setColumn(currentCube, tmp, TOP, 2, false);
+        this.rotate(currentCube, RIGHT, 1);
+        break;
+      default:
+        break;
     }
-    return cube;
+    return currentCube;
   }
 
   public String getColorName(byte color) {
@@ -249,17 +261,18 @@ class Operator implements Constants {
       case 0x5: name = "O"; break;
       case 0x6: name = "Y"; break;
     }
+
     return name;
   }
 
-  public void printCube(byte[] ncube) {
-    byte[] current = deepCopy(ncube);
+  public void printCube(byte[] current) {
     String strCube = "";
     String offset = "        ";
 
     // Print Top
     for (int i = 0; i < 9; i++) {
-      if (i % 3 == 0) strCube += "\n" + offset;
+      if (i % 3 == 0)
+        strCube += "\n" + offset;
       strCube += this.getColorName(current[((TOP * 9) + i)]) + " ";
     }
 
@@ -278,8 +291,11 @@ class Operator implements Constants {
 
     // Print Bottom
     for (int i = 0; i < 9; i++) {
-      if (i % 3 == 0 && i != 0) strCube += "\n";
-      if (i % 3 == 0) strCube += offset;
+      if (i % 3 == 0 && i != 0)
+        strCube += "\n";
+      if (i % 3 == 0)
+        strCube += offset;
+
       strCube += this.getColorName(current[((BOTTOM * 9) + i)]) + " ";
     }
 
